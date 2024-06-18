fd998ac35343640639742c2eab73db8187efbbde
albertcrz <albertcrz@LAPTOP-R1JQIB90>
2024-06-18 11:59:46 +0100

    pasarela

--- client/src/Completion.js
+++ client/src/Completion.js
@@ -1,8 +1,5 @@
 function Completion(props) {
   return <h1>Pago realizado con éxito ¡Gracias por su compra! 🎉</h1>;
 }
-<button>
- <span>Volver al Inicio</span>
-</button>
 
 export default Completion;
